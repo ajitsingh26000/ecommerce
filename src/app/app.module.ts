3f27c8064819dce6b32bf155ee4be9bc6c4425b5
Ajit Singh <ajitsingh26000@gmail.com>
2018-01-11 11:38:56 +0530

    csstoscss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/contactus/contactus.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Feedback } from '../shared/feedback';
+
+@Component({
+  selector: 'app-contactus',
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.scss']
+})
+export class ContactusComponent implements OnInit {
+  
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  validationMessages = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
